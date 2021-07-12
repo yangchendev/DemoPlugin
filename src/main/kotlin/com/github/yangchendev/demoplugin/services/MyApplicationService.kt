59b6d8a75df9ca257126b3c24e82c3474ab84145
GitHub Action <action@github.com>
2021-07-12 09:17:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/yangchendev/demoplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yangchendev/demoplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yangchendev.demoplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yangchendev.demoplugin.MyBundle
 
 class MyApplicationService {
 
